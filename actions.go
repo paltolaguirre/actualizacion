0b67542d9301360d356c02ef777e67a535e08d5d
Walter <wschmidt@xubio.com>
2020-05-27 11:01:55 -0300

    [feature/wschmidt-svc196] Primer version del microservicio de Actualizacion

---
+++ actions.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/xubiosueldos/conexionBD"
+	"github.com/xubiosueldos/conexionBD/Autenticacion/structAutenticacion"
+	"github.com/xubiosueldos/conexionBD/apiclientconexionbd"
+	"github.com/xubiosueldos/framework"
+	"net/http"
+)
+
+// Sirve para controlar si el server esta OK
+func Healthy(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Healthy."))
+}
+
+func Actualizar(writer http.ResponseWriter, request *http.Request) {
+	var err error
+	decoder := json.NewDecoder(request.Body)
+
+	var security structAutenticacion.Security
+
+	if err := decoder.Decode(&security); err != nil {
+		framework.RespondError(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	tenant := security.Tenant
+	db := conexionBD.ObtenerDB(tenant)
+	defer conexionBD.CerrarDB(db)
+	err = apiclientconexionbd.AutomigrateTablasPrivadas(db)
+
+	dbSecurity := conexionBD.ObtenerDB("security")
+	defer conexionBD.CerrarDB(dbSecurity)
+	if err != nil {
+		fmt.Println("Error Automigrate Tablas Privadas: ", err)
+		if err := dbSecurity.Unscoped().Where("token = ?", security.Token).Delete(structAutenticacion.Security{}).Error; err != nil {
+			framework.RespondError(writer, http.StatusInternalServerError, err.Error())
+		}
+		framework.RespondError(writer, http.StatusInternalServerError, fmt.Sprintf("%s: %s", framework.ErrorAutomigrate, err.Error()))
+		return
+	} else {
+		security.Necesitaupdate = false
+		dbSecurity.Model(&security).Where("tenant = ?", security.Tenant).Update("necesitaupdate", false)
+	}
+	framework.RespondJSON(writer, http.StatusOK, &security)
+	return
+}
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/xubiosueldos/conexionBD"
+	"github.com/xubiosueldos/conexionBD/Autenticacion/structAutenticacion"
+	"github.com/xubiosueldos/conexionBD/apiclientconexionbd"
+	"github.com/xubiosueldos/framework/configuracion"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var err error
+	configuracion := configuracion.GetInstance()
+
+	dbPublic := conexionBD.ObtenerDB("public")
+	err, actualizoMicro := apiclientconexionbd.AutomigrateTablasPublicas(dbPublic)
+	if err != nil {
+		fmt.Println("Error Public Automigrate: ", err)
+		return
+	}
+	conexionBD.CerrarDB(dbPublic)
+
+	dbSecurity := conexionBD.ObtenerDB("security")
+	txSecurity := dbSecurity.Begin()
+
+	err, actualizoSecurity := apiclientconexionbd.AutomigrateTablaSecurity(txSecurity)
+	if err != nil {
+		txSecurity.Rollback()
+		fmt.Println("Error Security Automigrate: ", err)
+		return
+	}
+
+	if actualizoMicro || actualizoSecurity {
+		cleanConnections(txSecurity)
+	}
+
+	txSecurity.Commit()
+	conexionBD.CerrarDB(dbSecurity)
+
+	router := newRouter()
+
+	fmt.Printf("Iniciando servidor escuchando puerto %s", configuracion.Puertomicroservicioactualizacion)
+	server := http.ListenAndServe(":"+configuracion.Puertomicroservicioactualizacion, router)
+
+	log.Fatal(server)
+
+}
+
+func cleanConnections(db *gorm.DB)  {
+	db.Model(&structAutenticacion.Security{}).Update("necesitaupdate", true)
+}
