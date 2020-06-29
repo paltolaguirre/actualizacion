73b37c2b7efdb3c33732ce7dec6599f33d2d7e5c
Walter <wschmidt@xubio.com>
2020-06-29 16:41:51 -0300

    [feature/wschmidt-svc196] Main mejor organizado

--- main.go
+++ main.go
@@ -9,61 +9,25 @@ import (
 	"github.com/xubiosueldos/framework/configuracion"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 
-	var err error
-
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
 	configuracion := configuracion.GetInstance()
 
-	dbPublic := conexionBD.ObtenerDB("public")
-	txPublic := dbPublic.Begin()
-	defer func() {
-		if r := recover(); r != nil {
-			txPublic.Rollback()
-			conexionBD.CerrarDB(dbPublic)
-		}
-	}()
+	err, actualizoMicro := actualizarTablasPublicas()
 
-	err, actualizoMicro := automigrate.AutomigrateTablasPublicas(txPublic)
 	if err != nil {
 		fmt.Println("Error Public Automigrate: ", err)
-		txPublic.Rollback()
 		return
 	}
-	txPublic.Commit()
 
-	conexionBD.CerrarDB(dbPublic)
+	err = actualizarSecurity(actualizoMicro)
 
-	dbSecurity := conexionBD.ObtenerDB("security")
-	txSecurity := dbSecurity.Begin()
-	defer func() {
-		if r := recover(); r != nil {
-			txSecurity.Rollback()
-			conexionBD.CerrarDB(dbSecurity)
-		}
-	}()
-
-	err, actualizoSecurity := automigrate.AutomigrateTablaSecurity(txSecurity)
 	if err != nil {
-		txSecurity.Rollback()
 		fmt.Println("Error Security Automigrate: ", err)
 		return
 	}
-
-	if actualizoMicro || actualizoSecurity {
-		cleanConnections(txSecurity)
-	}
-
-	txSecurity.Commit()
-	conexionBD.CerrarDB(dbSecurity)
 
 	router := newRouter()
 
@@ -76,4 +40,51 @@ func main() {
 
 func cleanConnections(db *gorm.DB)  {
 	db.Model(&structAutenticacion.Security{}).Update("necesitaupdate", true)
+}
+
+
+func actualizarTablasPublicas() (error, bool) {
+	dbPublic := conexionBD.ObtenerDB("public")
+	defer conexionBD.CerrarDB(dbPublic)
+
+	txPublic := dbPublic.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			txPublic.Rollback()
+		}
+	}()
+
+	err, actualizoMicro := automigrate.AutomigrateTablasPublicas(txPublic)
+	if err != nil {
+		txPublic.Rollback()
+		return err, actualizoMicro
+	}
+	txPublic.Commit()
+
+	return nil, actualizoMicro
+}
+
+func actualizarSecurity(actualizoMicro bool) error {
+	dbSecurity := conexionBD.ObtenerDB("security")
+	defer conexionBD.CerrarDB(dbSecurity)
+	txSecurity := dbSecurity.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			txSecurity.Rollback()
+		}
+	}()
+
+	err, actualizoSecurity := automigrate.AutomigrateTablaSecurity(txSecurity)
+	if err != nil {
+		txSecurity.Rollback()
+		return err
+	}
+
+	if actualizoMicro || actualizoSecurity {
+		cleanConnections(txSecurity)
+	}
+
+	txSecurity.Commit()
+
+	return nil
 }
