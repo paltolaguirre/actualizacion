e2410350fc1d42458264b9dd1c78245f81564ee9
Walter <wschmidt@xubio.com>
2020-07-01 13:18:42 -0300

    [feature/wschmidt-svc196] Elimino lo de pau para que lo actualice en su rama

--- resources/concepto/public-20.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-    BEGIN
-        INSERT INTO TIPOIMPUESTOGANANCIAS(id,created_at, nombre, codigo, descripcion, activo, aplicaimporteremunerativo, aplicaimportenoremunerativo,aplicadescuento, aplicaretencion,aplicaaportepatronal) VALUES(-17,current_timestamp,'SAC Primer Cuota Exentas/No Alcanzadas','SAC_PRIMER_CUOTA_EXENTAS_NO_ALCANZADAS','',1, true, true, true, true, false);
-        INSERT INTO TIPOIMPUESTOGANANCIAS(id,created_at, nombre, codigo, descripcion, activo, aplicaimporteremunerativo, aplicaimportenoremunerativo,aplicadescuento, aplicaretencion,aplicaaportepatronal) VALUES(-18,current_timestamp,'SAC Segunda Cuota Exentas/No Alcanzadas','SAC_SEGUNDA_CUOTA_EXENTAS_NO_ALCANZADAS','',1, true, true, true, true, false);
-        INSERT INTO TIPOIMPUESTOGANANCIAS(id,created_at, nombre, codigo, descripcion, activo, aplicaimporteremunerativo, aplicaimportenoremunerativo,aplicadescuento, aplicaretencion,aplicaaportepatronal) VALUES(-19,current_timestamp,'Retribuciones No Habituales Exentas/No Alcanzadas','RETRIBUCIONES_NO_HABITUALES_EXENTAS_NO_ALCANZADAS','',1, true, true, true, false, false);
-        INSERT INTO TIPOIMPUESTOGANANCIAS(id,created_at, nombre, codigo, descripcion, activo, aplicaimporteremunerativo, aplicaimportenoremunerativo,aplicadescuento, aplicaretencion,aplicaaportepatronal) VALUES(-20,current_timestamp,'Ajustes Períodos Anteriores - Remuneraciones Gravadas','AJUSTES_PERÍODOS_ANTERIORES_REMUNERACIONES_GRAVADAS','',1, true, true, true, false, false);
-        INSERT INTO TIPOIMPUESTOGANANCIAS(id,created_at, nombre, codigo, descripcion, activo, aplicaimporteremunerativo, aplicaimportenoremunerativo,aplicadescuento, aplicaretencion,aplicaaportepatronal) VALUES(-21,current_timestamp,'Ajustes Períodos Anteriores - Remuneraciones Exentas/No Alcanzadas','AJUSTES_PERÍODOS_ANTERIORES_REMUNERACIONES_EXENTAS_NO_ALCANZADAS','',1, true, true, true, false, false);
-    end;
-$$;
